86d08f2a3629d51538cd4920f51f685cc8f4ac90
Camila-Negron <carla.negron@ucb.edu.bo>
2022-11-02 22:14:49 -0400

    version2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MotosComponent } from './motos/motos.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'motos',
+    component:MotosComponent
+  },
+  {
+    path:'formulario',
+    component:FormularioComponent
+  },
+  {
+    path:'contactos',
+    component:ContactosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { MotosComponent } from './motos/motos.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    MotosComponent,
+    FormularioComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
